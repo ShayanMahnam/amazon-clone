c480bfaba60c3a062b57a3a8a70838ebefb01342
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-04-06 20:57:39 +0100

    added basket home page header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
-import './App.css';
+import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
+import Checkout from "./Checkout";
+// import Login from "./Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Header />
+        <Routes>
+          {/* <Route path="/login">
+            <Login />
+          </Route> */}
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
